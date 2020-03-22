58ee6c1f7c0be5c4f624c0efb12606e92ad2fdb1
Chris Van Law <chris@chrisvanlaw.com>
2020-03-22 13:25:17 -0600

    feat: finish day 1 and bonus

--- src/ruby/day_1.rb
+++ src/ruby/day_1.rb
@@ -16,6 +16,17 @@ def increment_sentence
     end
 end
 
+def number_guesser
+    numberToGuess = rand(11)
+    puts numberToGuess
+    puts 'Guess a number from 0-10:'
+    guess = gets.to_i
+    puts "You guessed #{guess}"
+    puts 'That\'s right!' if guess == numberToGuess
+    puts 'Too low' if guess < numberToGuess
+    puts 'Too high' if guess > numberToGuess    
+end
+
 puts 'Hello, World'
 
 print_my_name
@@ -24,3 +35,5 @@ hello = 'Hello, Ruby'
 puts hello.index('Ruby')
 
 increment_sentence
+
+number_guesser
